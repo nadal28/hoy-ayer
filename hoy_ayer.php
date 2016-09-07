41b1597229d5632e41d6ffce2af652908672a660
nadal28 <nadal_28@hotmail.es>
2016-09-08 00:10:32 +0200

    Update hoy_ayer.php

--- hoy_ayer.php
+++ hoy_ayer.php
@@ -1,12 +1,12 @@
 function hoy_ayer($tiempo){
 
-	$dia = date('d-m-Y',$tiempo);
-	$hoy = date('d-m-Y');
+$dia = date('d-m-Y',$tiempo);
+$hoy = date('d-m-Y');
 
-	if($dia == $hoy)
-		return 'Hoy, '.date('H:i',$row['ult_vez']);
-	elseif(strtotime($hoy) - strtotime($dia) == 86400)
-		return 'Ayer, '.date('H:i',$row['ult_vez']);
-	else
-		return date('H:i d-m-Y', $row['ult_vez']);
+if($dia == $hoy)
+	return 'Hoy, '.date('H:i',$tiempo);
+elseif(strtotime($hoy) - strtotime($dia) == 86400)
+	return 'Ayer, '.date('H:i',$tiempo);
+else
+	return date('H:i d-m-Y', $tiempo);
 }
